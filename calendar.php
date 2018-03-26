76fe679b72213469dbcf50f3938645679a15de3c
thrishma <thrishmareddy@gmail.com>
2018-03-25 20:28:51 -0400

    css changes

--- calendar.php
+++ calendar.php
@@ -5,17 +5,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link href="css/calendar.css" rel="stylesheet">
-    <title>Period Calendar</title>
-
+   
+     <title>Period Calendar</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/start/jquery-ui.min.css" rel="stylesheet">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">   
+   
+
+    
+    <!--Style-Sheets -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/calendar.css">
+    
+    <link rel="icon" type="image/png" href="img/logo.png" />
+
+    <!-- font family -->
+    <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+
     <style>
     .dp-highlight .ui-state-default {
         background: #484;
@@ -24,20 +35,15 @@
 
     </style>
 
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <link rel="stylesheet" type="text/css" href="css/calendar.css">
-
-    <link rel="icon" type="image/png" href="img/logo.png" />
-
-    <!-- font family -->
-    <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+    
 </head>
 <body>
+    <?php include('menu.php') ?>
     <center>
         <div class="main_wrapper">
            <div class="header">
                 <h2>Period Tracker</h2>
-                <p>Wondering, "When will I get my period?" <span style="color:red";>Just Flow</span> knows! Our easy tracking tool helps map out your cycle for months. Plan a period-free beach trip or a big event like a wedding.</p>
+                <p>Wondering, "When will I get my period?" <span style="color:red;">Just Flow</span> knows! Our easy tracking tool helps map out your cycle for months. Plan a period-free beach trip or a big event like a wedding.</p>
                      
             </div>
             <div id="user_name" data-prodnumber="${sessionScope.username}" />
@@ -103,9 +109,9 @@
         </div>
 
     </center>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+     
+    
 </body>
 </html>
