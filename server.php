85f12f017a7b302f6445f938d7b8e24aebcd1ec6
thrishma <thrishmareddy@gmail.com>
2018-03-28 19:22:39 -0400

    single server login

--- server.php
+++ server.php
@@ -1,22 +1,22 @@
 <?php 
 	session_start();
 
-	// variable declaration
-	$username = "";
+	// variable declaration	
 	$email    = "";
-	$errors = array(); 
+	$errors = array();
+	$confirmationEMail = array(); 
 	$_SESSION['success'] = "";
 
 	// connect to database
-	$db = mysqli_connect('localhost', 'root', '', 'registration');
+	include 'serverLoginDetails.php';
 
 	// REGISTER USER
 	if (isset($_POST['reg_user'])) {
 		// receive all input values from the form
-		$username = mysqli_real_escape_string($db, $_POST['username']);
-		$email = mysqli_real_escape_string($db, $_POST['email']);
-		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+		$username = mysqli_real_escape_string($conn, $_POST['username']);
+		$email = mysqli_real_escape_string($conn, $_POST['email']);
+		$password_1 = mysqli_real_escape_string($conn, $_POST['password_1']);
+		$password_2 = mysqli_real_escape_string($conn, $_POST['password_2']);
 
 		// form validation: ensure that the form is correctly filled
 		if (empty($username)) { array_push($errors, "Username is required"); }
@@ -27,8 +27,8 @@
 			array_push($errors, "Passwords do not match");
 		}
 
-		$check_if_user_name = mysqli_query($db,"SELECT * FROM users WHERE username='$username'");
-		$check_if_user_email = mysqli_query($db,"SELECT * FROM users WHERE email='$email'");
+		$check_if_user_name = mysqli_query($conn,"SELECT * FROM users WHERE username='$username'");
+		$check_if_user_email = mysqli_query($conn,"SELECT * FROM users WHERE email='$email'");
 
 		if(mysqli_num_rows($check_if_user_name) > 0){ array_push($errors, "User name has been taken");}
 		if(mysqli_num_rows($check_if_user_email) > 0){ array_push($errors, "Email Id is already registered with us");}
@@ -37,21 +37,21 @@
 		if (count($errors) == 0) {
 			$password = md5($password_1);//encrypt the password before saving in the database
 			$confirmcode = rand();
-			$query = "INSERT INTO users (id,username, email, password, confirmed, confirmed_code) 
-					  VALUES('','$username', '$email', '$password', '0', '$confirmcode')";
+			$query = "INSERT INTO users VALUES('','$username', '$email', '$password', '0', '$confirmcode')";
 
-			$message = "Please confirm your Email
-						Click the below link to verify your account
-						https://www.justflow.com/emailconfirmation.php?username=$username&code=$confirmcode";
+			$message = "Hey '$username' thank you for Registering with Just FLow - The goto Menstural Application
 
-			mail($email, "Just Flow Confirm Email", $message, "From: DoNotReply@justflow.com");
-			echo "Registration complete! Please confirm your email";
+Please confirm your Email by clicking the below link to verify your account
+https://www.thrishma.com/justflow_web/confirmLink.php?username=$username&code=$confirmcode";
 
-			mysqli_query($db, $query);
+			mail($email, "Just Flow Confirmation Email", $message, "From: justflow@thrishma.com");
+			array_push($confirmationEmail, "Registration complete! Please confirm your email");
+
+			mysqli_query($conn, $query);
 
 			$_SESSION['username'] = $username;
-			$_SESSION['success'] = "You are now logged in";			
-			header('location: questions.php');
+			$_SESSION['success'] = "You are now logged in";						
+			header('location: login.php');
 		}
 
 	}
@@ -60,8 +60,8 @@
 
 	// LOGIN USER
 	if (isset($_POST['login_user'])) {
-		$username = mysqli_real_escape_string($db, $_POST['username']);
-		$password = mysqli_real_escape_string($db, $_POST['password']);
+		$username = mysqli_real_escape_string($conn, $_POST['username']);
+		$password = mysqli_real_escape_string($conn, $_POST['password']);
 
 		if (empty($username)) {
 			array_push($errors, "Username is required");
@@ -69,16 +69,37 @@
 		if (empty($password)) {
 			array_push($errors, "Password is required");
 		}
+		if (!(empty($username) || empty($password))) {
+		$query = "SELECT confirmed FROM users WHERE username='$username'";
+		$results = mysqli_query($conn, $query);
+		while($row = mysqli_fetch_array($results)){
+				if (mysqli_num_rows($results) == 1){
+					if($row['confirmed'] == 0){
+						array_push($errors, "Email Id has not been verified yet. Please verify your Email Id");
+					}
+				}
+			}
+		}
 
 		if (count($errors) == 0) {
 			$password = md5($password);
 			$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-			$results = mysqli_query($db, $query);
+			$results = mysqli_query($conn, $query);
 
 			if (mysqli_num_rows($results) == 1) {
 				$_SESSION['username'] = $username;
-				$_SESSION['success'] = "You are now logged in";				
-				header('location: features.php');
+				$_SESSION['success'] = "You are now logged in";	
+				$query_login_first = "SELECT * FROM users WHERE username='$username'";
+				$query_login_first_results = mysqli_query($conn, $query_login_first);
+				while($row = mysqli_fetch_array($query_login_first_results)){
+					if (mysqli_num_rows($results) == 1){
+						if(($row['confirmed']) == 0){
+						header('location: questions.php');
+						}else{
+							header('location: features.php');
+						}
+					}
+				}				
 			}else {
 				array_push($errors, "Wrong username/password combination");
 			}
---
+++ serverLoginDetails.php
@@ -0,0 +1,15 @@
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "just_flow";
+
+	// $uservername = "localhost";
+	// $username = "thriyyqo_thrishma";
+	// $password = "justflow2823";
+	// $dbname = "thriyyqo_justflow";
+
+	$conn=mysqli_connect($servername,$username,$password,$dbname);	
+	
+?>
